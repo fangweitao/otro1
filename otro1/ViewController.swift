1e689a863ab9d652bf91aef28cea99928034bcbd
Christian Fernandez <kistian@hotmail.com>
2018-11-16 02:32:58 +0800

    Initial Commit

---
+++ otro1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  otro1
+//
+//  Created by Christian Fernandez on 2018/11/16.
+//  Copyright © 2018 Christian Fernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
---
+++ otro1Tests/otro1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  otro1Tests.swift
+//  otro1Tests
+//
+//  Created by Christian Fernandez on 2018/11/16.
+//  Copyright © 2018 Christian Fernandez. All rights reserved.
+//
+
+import XCTest
+@testable import otro1
+
+class otro1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
